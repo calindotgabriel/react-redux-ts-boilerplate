105a75db501ab22c7497abc173276c05519b1daf
cgabriel <calindotgabriel18@gmail.com>
2022-01-09 11:48:06 +0200

    redux react ts boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
+import { ArticleState, IArticle } from './model/article';
+import { Dispatch } from "redux"
+import { addArticle, removeArticle } from './store/actionCreators';
+import { AddArticle } from './components/AddArticle';
+import { Article } from './components/Article';
 
 function App() {
+  const articles: readonly IArticle[] = useSelector(
+    (state: ArticleState) => state.articles,
+    shallowEqual
+  );
+  
+  const dispatch: Dispatch<any> = useDispatch()
+  
+  const saveArticle = React.useCallback(
+    (article: IArticle) => dispatch(addArticle(article)),
+    [dispatch]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <h1>My Articles</h1>
+    <AddArticle saveArticle={saveArticle} />
+    {articles.map((article: IArticle) => (
+      <Article
+        key={article.id}
+        article={article}
+        removeArticle={removeArticle}
+      />
+    ))}
+  </main>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, Store } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reducer from "./store/reducer"
+import { ArticleAction, ArticleState, DispatchType } from './model/article';
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
